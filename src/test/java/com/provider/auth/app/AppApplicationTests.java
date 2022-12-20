a830143544226559aa0462f6de74e05a245fdcc6
Bruno Fernandes <brunofernandes100@hotmail.com.br>
2022-12-20 16:44:38 -0300

    Initial commit

---
+++ src/test/java/com/provider/auth/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provider.auth.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
